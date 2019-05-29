607046e78d387350c82af9f0f8114c31300ae264
shaopigan <243107208@qq.com>
2019-05-29 23:09:28 +0800

    pc manage front

---
+++ src/api/data.js
@@ -0,0 +1,61 @@
+import axios from '@/libs/api.request'
+
+//调取用户列表接口
+export const getUserTableData = () => {
+  return axios.request({  // 这里返回的是一个Promise，request方法传入一个配置对象，配置项可参考axios
+    url: '/thinkphp5/public/get_users_table_data',
+    method: 'get'
+  })
+}
+
+
+
+export const getTableData = () => {
+  return axios.request({
+    url: 'get_table_data',
+    method: 'get'
+  })
+}
+
+export const getDragList = () => {
+  return axios.request({
+    url: 'get_drag_list',
+    method: 'get'
+  })
+}
+
+export const errorReq = () => {
+  return axios.request({
+    url: 'error_url',
+    method: 'post'
+  })
+}
+
+export const saveErrorLogger = info => {
+  return axios.request({
+    url: 'save_error_logger',
+    data: info,
+    method: 'post'
+  })
+}
+
+export const uploadImg = formData => {
+  return axios.request({
+    url: 'image/upload',
+    data: formData
+  })
+}
+
+export const getOrgData = () => {
+  return axios.request({
+    url: 'get_org_data',
+    method: 'get'
+  })
+}
+
+export const getTreeSelectData = () => {
+  return axios.request({
+    url: 'get_tree_select_data',
+    method: 'get'
+  })
+}
---
+++ src/view/users/users.vue
@@ -0,0 +1,46 @@
+<template>
+  <Table :columns="columns1" :data="tableData"></Table>
+</template>
+
+<script>
+  import { getUserTableData } from "../../api/data";
+
+  export default {
+    name: "usersList",
+    data(){
+      return {
+        tableData:[
+        ],
+        columns1: [
+          {
+            title: 'Name',
+            key: 'name'
+          },
+          {
+            title: 'Age',
+            key: 'age'
+          },
+          {
+            title: 'Phone',
+            key: 'phone'
+          }
+        ],
+      }
+    },
+    methods:{
+
+    },
+    mounted(){
+      getUserTableData().then(res => {
+        // console.log(res);
+        this.tableData = res.data.data;
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
